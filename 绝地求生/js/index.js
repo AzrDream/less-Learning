1d1a79fb4ce54489116ff685882b17a5d7603d8d
AzrDream <1499325862@qq.com>
2022-07-03 15:14:47 +0800

    绝地求生结构搭建

---
+++ 绝地求生/js/index.js
@@ -0,0 +1,6 @@
+window.onload = function () {
+    new fullpage('#fullpage',{
+        verticalCentered: false,
+        sectionsColor: ['#f00', '#0f0', '#00f', '#ff0', '#0ff', '#f0f', '#000'],
+    })
+};

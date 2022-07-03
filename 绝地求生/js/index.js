7eb5823405e23c573ef0db395d90560e69f70c96
AzrDream <1499325862@qq.com>
2022-07-03 15:25:02 +0800

    绝地求生吸顶效果

--- 绝地求生/js/index.js
+++ 绝地求生/js/index.js
@@ -1,6 +1,21 @@
 window.onload = function () {
+    // 1.拿到需要操作的元素
+    let oToolbar = document.querySelector(".toolbar");
+    let oNav = document.querySelector(".nav");
+
     new fullpage('#fullpage',{
         verticalCentered: false,
         sectionsColor: ['#f00', '#0f0', '#00f', '#ff0', '#0ff', '#f0f', '#000'],
+        onLeave: function (origin, destination, direction) {
+            if(destination.isFirst) {
+                // 是第一屏
+                oToolbar.style.display = "block";
+                oNav.style.top = "42px";
+            }else{
+                // 不是第一屏
+                oToolbar.style.display = "none";
+                oNav.style.top = "0px";
+            }
+        }
     })
 };

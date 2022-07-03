f98e8504997d7083d476350bf71482057685bedd
AzrDream <1499325862@qq.com>
2022-07-03 17:37:04 +0800

    绝地求生第四屏

--- 绝地求生/js/index.js
+++ 绝地求生/js/index.js
@@ -32,5 +32,54 @@ window.onload = function () {
                 oTips.style.display = "block";
             }
         }
-    })
+    });
+
+    // 3.监听菜单的点击事件
+    oMenuUp.onclick = function () {
+        fullpage_api.moveSectionUp();
+    };
+    oMenuDown.onclick = function () {
+        fullpage_api.moveSectionDown();
+    };
+
+    // 4.初始化第四屏动画
+    initSection4Animation();
 };
+function initSection4Animation() {
+    // 1.拿到需要操作的元素
+    let oLis = document.querySelectorAll(".section-four>ul>li");
+    let oImages = document.querySelectorAll(".section-four>ul>li>img");
+    let oH3 = document.querySelectorAll(".section-four>ul>li>h3");
+
+    // 2.给所有的li添加移入移出事件
+    for(let i = 0; i < oLis.length; i++){
+        let item = oLis[i];
+        // 移入事件
+        item.onmouseenter = function () {
+            oLis[0].style.width = "20%";
+            oLis[1].style.width = "20%";
+            oLis[2].style.width = "20%";
+            oLis[i].style.width = "60%";
+            oImages[i].style.opacity = "1";
+            oH3[i].style.opacity = "0";
+            if(i === 0){
+                oImages[0].style.left = "0";
+            }else if(i === 2){
+                oImages[2].style.right = "0";
+            }
+        };
+        // 移出事件
+        item.onmouseleave = function () {
+            oLis[0].style.width = "33%";
+            oLis[1].style.width = "34%";
+            oLis[2].style.width = "33%";
+            oImages[i].style.opacity = "0.6";
+            oH3[i].style.opacity = "1";
+            if(i === 0){
+                oImages[0].style.left = "-180px";
+            }else if(i === 2){
+                oImages[2].style.right = "-180px";
+            }
+        }
+    }
+}
